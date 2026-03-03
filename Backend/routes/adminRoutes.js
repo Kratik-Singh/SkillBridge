7d134bb4dbe1fa28d0a04ad1ea701afe25a50402
Kratik Singh Kushwah <kratikkushwah2006@gmail.com>
2026-03-03 20:35:32 +0530

    Fix import case for User model in adminRoutes

--- Backend/routes/adminRoutes.js
+++ Backend/routes/adminRoutes.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const authMiddleware = require("../middleware/authMiddleware");
 const adminMiddleware = require("../middleware/adminMiddleware");
-const User = require("../models/User");
+const User = require("../models/user");
 
 // GET ALL USERS
 router.get("/users",
