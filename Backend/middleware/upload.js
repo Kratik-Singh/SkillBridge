fe319853444d2ce0c279cd7373ef63c2c4417e49
Kratik-Singh <kratikkushwah2006@gmail.com>
2026-04-01 22:47:53 +0530

    error correction

--- Backend/middleware/upload.js
+++ Backend/middleware/upload.js
@@ -1,12 +1,20 @@
-const multer = require('multer');
-const { CloudinaryStorage } = require('multer-storage-cloudinary');
-const cloudinary = require('../config/cloudinary'); // your config file
+const multer = require("multer");
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
 
+// Cloudinary config
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
+
+// Storage config
 const storage = new CloudinaryStorage({
   cloudinary: cloudinary,
   params: {
-    folder: 'uploads',
-    allowed_formats: ['jpg', 'png', 'jpeg'],
+    folder: "skillbridge_profiles",
+    allowed_formats: ["jpg", "png", "jpeg"],
   },
 });
 
